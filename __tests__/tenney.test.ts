37b4627606f956ae8c35ddcd7599af31eee660c0
Andrew C. Smith <andrewchristophersmith@gmail.com>
2020-06-06 11:35:28 -0700

    test harmonicDistance of multiple intervals

--- __tests__/tenney.test.ts
+++ __tests__/tenney.test.ts
@@ -2,9 +2,19 @@ import { harmonicDistance } from "../src/tenney"
 import * as tf from "@tensorflow/tfjs-node-gpu"
 
 const FIFTH = tf.tensor([[-1, 1, 0]])
+const TRIAD = tf.tensor([
+    [-2, 0, 1],
+    [-1, 1, 0]
+])
 
 test('harmonicDistance of one interval', async () => {
     const exp = tf.tensor([2.584962500721156])
     const res = harmonicDistance(FIFTH)
+    expect(await res.sub(exp).sum().array()).toBeLessThan(1.0e-5)
+})
+
+test('harmonicDistance of multiple intervals', async () => {
+    const exp = tf.tensor([4.321928094887363, 2.584962500721156])
+    const res = harmonicDistance(TRIAD)
     expect(await res.sub(exp).sum().array()).toBeLessThan(1.0e-5)
 })
