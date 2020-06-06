44f2b161259c7cc78cebf6af07d02e18d8297364
Andrew C. Smith <andrewchristophersmith@gmail.com>
2020-06-06 11:51:40 -0700

    implement harmonicDistanceAggregate

--- __tests__/tenney.test.ts
+++ __tests__/tenney.test.ts
@@ -1,4 +1,8 @@
-import { harmonicDistance } from "../src/tenney"
+import { 
+    harmonicDistance, 
+    harmonicDistanceAggregate 
+} from "../src/tenney"
+
 import * as tf from "@tensorflow/tfjs-node-gpu"
 
 const FIFTH = tf.tensor([[-1, 1, 0]])
@@ -6,15 +10,35 @@ const TRIAD = tf.tensor([
     [-2, 0, 1],
     [-1, 1, 0]
 ])
+const UNISON_FIFTH = tf.tensor([
+    [0, 0, 0],
+    [-1, 1, 0]
+])
+
+async function expectInRange(res, exp, range = 1.0e-5) {
+    expect(await res.sub(exp).sum().array()).toBeLessThan(range)
+}
 
 test('harmonicDistance of one interval', async () => {
     const exp = tf.tensor([2.584962500721156])
     const res = harmonicDistance(FIFTH)
-    expect(await res.sub(exp).sum().array()).toBeLessThan(1.0e-5)
+    expectInRange(res, exp)
 })
 
 test('harmonicDistance of multiple intervals', async () => {
     const exp = tf.tensor([4.321928094887363, 2.584962500721156])
     const res = harmonicDistance(TRIAD)
-    expect(await res.sub(exp).sum().array()).toBeLessThan(1.0e-5)
+    expectInRange(res, exp)
+})
+
+test('harmonicDistance aggregate of UNISON_FIFTH', async () => {
+    const exp = tf.tensor([2.584962500721156])
+    const res = harmonicDistanceAggregate(UNISON_FIFTH)
+    expectInRange(res, exp)
+})
+
+test('harmonicDistance aggregate of TRIAD', async () => {
+    const exp = tf.tensor([5.9068906])
+    const res = harmonicDistanceAggregate(TRIAD)
+    expectInRange(res, exp)
 })
--- src/tenney.ts
+++ src/tenney.ts
@@ -1,14 +1,29 @@
 import * as tf from "@tensorflow/tfjs-node-gpu"
 import { PRIMES } from "./vectors"
+import { getBases } from "./utilities"
 
-const harmonicDistance = (vecs: tf.Tensor): tf.Tensor => {
+const explodedHarmonicDistance = (vecs: tf.Tensor): tf.Tensor => {
     let out = PRIMES.slice(0, vecs.shape.slice(-1))
     out = tf.pow(out, vecs)
-    out = out.log().div(tf.log(2.0))
-    out = tf.sum(tf.abs(out), 1)
-    return out
+    return out.log().div(tf.log(2.0))
+}
+
+const harmonicDistance = (vecs: tf.Tensor): tf.Tensor => {
+    return tf.sum(tf.abs(explodedHarmonicDistance(vecs)), 1)
+}
+
+const harmonicDistanceAggregate = async (vecs: tf.Tensor): Promise<tf.Tensor> => {
+    const bases = await getBases(vecs.shape[1] + 1)
+    return explodedHarmonicDistance(vecs)
+        .transpose()
+        .dot(bases)
+        .abs()
+        .sum(0)
+        .sum(0)
+        .div(vecs.shape[0])
 }
 
 export {
-    harmonicDistance
+    harmonicDistance,
+    harmonicDistanceAggregate,
 }
