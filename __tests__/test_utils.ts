2fc0bf865d0ac009ca240ce9c476b3e272cda0a8
Andrew C. Smith <andrewchristophersmith@gmail.com>
2020-07-25 14:52:43 -0700

    add default epsilon to test utils

--- __tests__/test_utils.ts
+++ __tests__/test_utils.ts
@@ -1,9 +1,9 @@
 import * as tf from "@tensorflow/tfjs-node-gpu"
 
-const expectTensorsClose = async (res: tf.Tensor, exp: tf.Tensor, close = true) => {
+const expectTensorsClose = async (res: tf.Tensor, exp: tf.Tensor, close = true, epsilon = 0.001) => {
     tf.test_util.expectArraysEqual(res.shape, exp.shape)
     if (close) {
-        tf.test_util.expectArraysClose(await res.array(), await exp.array())
+        tf.test_util.expectArraysClose(await res.array(), await exp.array(), epsilon)
     } else {
         tf.test_util.expectArraysEqual(await res.array(), await exp.array())
     }
